8f3ec26e031af61190fbcc328181e4b569464012
shayyo <shay.yosef@gmail.com>
2023-12-25 12:03:18 +0200

    Create get_token.py

---
+++ get_token.py
@@ -0,0 +1,10 @@
+import requests
+import sys
+
+HOST_URL = sys.argv[1]
+USERNAME = sys.argv[2]
+PASSWORD = sys.argv[3]
+
+r = requests.post(f"http://{HOST_URL}:8080/api/v1/login", json={"id":f"{USERNAME}", "password":f"{PASSWORD}"})
+token = r.json().get('token')
+print(token)
