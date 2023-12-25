2a0f329f3bbedb154c1fd68d71975a3653fbdcac
shayyo <shay.yosef@gmail.com>
2023-12-25 17:56:47 +0200

    Update get_token.py

--- get_token.py
+++ get_token.py
@@ -6,5 +6,8 @@ USERNAME = sys.argv[2]
 PASSWORD = sys.argv[3]
 
 r = requests.post(f"http://{HOST_URL}:8080/api/v1/login", json={"id":f"{USERNAME}", "password":f"{PASSWORD}"})
-token = r.json().get('token')
-print(f"The token is: {token}")
+TOKEN = r.json().get('token')
+print(f"The token is: {TOKEN}")
+
+HEADERS = {"Accept": "application/json", "aqua-auth" : f"Bearer {TOKEN}"}
+i = requests.post(f"http://{HOST_URL}:8080/api/v1/scanner/registry/Docker%20Hub/image/centos:8/scan", headers=HEADERS)
