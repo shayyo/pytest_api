be3e9574fbf1c86a85093e2f89d57a7036dff2ad
shayyo <shay.yosef@gmail.com>
2023-12-25 12:48:39 +0200

    Update get_token.py

--- get_token.py
+++ get_token.py
@@ -7,4 +7,4 @@ PASSWORD = sys.argv[3]
 
 r = requests.post(f"http://{HOST_URL}:8080/api/v1/login", json={"id":f"{USERNAME}", "password":f"{PASSWORD}"})
 token = r.json().get('token')
-print(token)
+print(f"The token is: {token}")
