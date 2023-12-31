673e14248843c67bfee254ae7a7386aa3be4ed0a
shayyo <shay.yosef@gmail.com>
2023-12-31 16:40:59 +0200

    Update get_token.py

--- get_token.py
+++ get_token.py
@@ -11,3 +11,4 @@ print(f"The token is: {TOKEN}")
 
 HEADERS = {"Accept": "application/json", "aqua-auth" : f"Bearer {TOKEN}"}
 i = requests.post(f"http://{HOST_URL}:8080/api/v1/scanner/registry/Docker%20Hub/image/centos:8/scan", headers=HEADERS)
+print(f"END OF SCRIPT {i}")
