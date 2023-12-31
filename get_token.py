3e1d60f8b5c52a5d7b1c7f258c37d66dc0ed9eac
shayyo <shay.yosef@gmail.com>
2023-12-31 17:13:47 +0200

    Update get_token.py

--- get_token.py
+++ get_token.py
@@ -7,7 +7,6 @@ PASSWORD = sys.argv[3]
 
 r = requests.post(f"http://{HOST_URL}:8080/api/v1/login", json={"id":f"{USERNAME}", "password":f"{PASSWORD}"})
 TOKEN = r.json().get('token')
-print(f"The token is: {TOKEN}")
 
 # HEADERS = {"Accept": "application/json", "aqua-auth": f"Bearer {TOKEN}", "Authorization": f"Bearer {TOKEN}"}
 
