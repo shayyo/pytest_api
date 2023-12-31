c327573d2c0aab97d4853021585daa0855d6cdc7
shayyo <shay.yosef@gmail.com>
2023-12-31 16:50:04 +0200

    Update get_token.py

--- get_token.py
+++ get_token.py
@@ -10,5 +10,9 @@ TOKEN = r.json().get('token')
 print(f"The token is: {TOKEN}")
 
 HEADERS = {"Accept": "application/json", "aqua-auth" : f"Bearer {TOKEN}"}
+
 i = requests.post(f"http://{HOST_URL}:8080/api/v1/scanner/registry/Docker%20Hub/image/centos:8/scan", headers=HEADERS)
-print(f"END OF SCRIPT {i}")
+print(i)
+
+r = requests.get(f"http://{HOST_URL}:8080/api/v2/containers?status=running&container_type=containers&page=1&pagesize=50, headers=HEADERS)
+print(r)
