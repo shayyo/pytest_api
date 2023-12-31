079a58b922c56a3cb10b367a2be2981d2c31aef5
shayyo <shay.yosef@gmail.com>
2023-12-31 17:16:30 +0200

    Update add_image_scan.py

--- add_image_scan.py
+++ add_image_scan.py
@@ -1,1 +1,15 @@
+import requests
+import sys
 
+TOKEN = sys.argv[1]
+
+
+
+# HEADERS = {"Accept": "application/json", "aqua-auth": f"Bearer {TOKEN}", "Authorization": f"Bearer {TOKEN}"}
+
+# i = requests.post(f"http://{HOST_URL}:8080/api/v1/scanner/registry/Docker%20Hub/image/centos:8/scan", headers=HEADERS)
+# if i.status_code == 200:
+#     print("Success")
+# else:
+#     print("Error")
+#     sys.exit(1)
