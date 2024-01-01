2297ca183f33da6cdc60c645603b0feac44746fc
shayyo <shay.yosef@gmail.com>
2024-01-01 11:01:07 +0200

    Update add_image_scan.py

--- add_image_scan.py
+++ add_image_scan.py
@@ -4,12 +4,12 @@ import sys
 TOKEN = sys.argv[1]
 
 
+def add_image_for_scanning(token, image):
+    HEADERS = {"Accept": "application/json", "aqua-auth": f"Bearer {TOKEN}", "Authorization": f"Bearer {TOKEN}"}
 
-# HEADERS = {"Accept": "application/json", "aqua-auth": f"Bearer {TOKEN}", "Authorization": f"Bearer {TOKEN}"}
-
-# i = requests.post(f"http://{HOST_URL}:8080/api/v1/scanner/registry/Docker%20Hub/image/centos:8/scan", headers=HEADERS)
-# if i.status_code == 200:
-#     print("Success")
-# else:
-#     print("Error")
-#     sys.exit(1)
+    i = requests.post(f"http://{HOST_URL}:8080/api/v1/scanner/registry/Docker%20Hub/image/centos:8/scan", headers=HEADERS)
+    if i.status_code == 200:
+        print("Success")
+    else:
+        print("Error")
+        sys.exit(1)
