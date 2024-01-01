9baf883c63ccae6d6e75d78058ce9bb9bfe4e196
shayyo <shay.yosef@gmail.com>
2024-01-01 11:26:47 +0200

    Update get_token.py

--- get_token.py
+++ get_token.py
@@ -1,5 +1,6 @@
 import requests
 import sys
+import json
 
 HOST_URL = sys.argv[1]
 USERNAME = sys.argv[2]
@@ -10,3 +11,6 @@ TOKEN = r.json().get('token')
 
 HEADERS = {"Accept": "application/json", "Authorization": f"Bearer {TOKEN}"}
 
+# add image for scanning
+image_data = '{"images":[{"registry":"Docker Hub","repository":"centos:latest","tag":"latest","digest":null,"source":null,"exists":false,"cf_app_guid":"","cf_space":"","cf_org":""}]}'
+r = requests.post(f"http://{HOST_URL}:8080/api/v1/images', headers=HEADERS, json=json.loads(image_data)) 
